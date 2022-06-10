0ca2187e6ed86cbb90bf80088373584eae056f11
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-06-10 13:35:41 +0300

    feat: сделана навигация для /movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,2 +1,9 @@
+import React from "react";
+import './Movies.css';
+import Navigation from "../Navigation/Navigation";
+
 export function Movies() {
+    return(
+        <Navigation />
+    )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,32 @@
 import React from "react";
 import './Navigation.css';
+import Logo from "../Logo/Logo";
+import {NavLink} from "react-router-dom";
+import icon from '../../images/icon.svg'
+
 
 export default function Navigation() {
+    const setActive = ({isActive}) =>
+        isActive
+            ? "navigation-header__link navigation-header__link_active link-opacity"
+            : "navigation-header__link link-opacity";
+
+    return (
+        <section className="navigation-header">
+            <Logo/>
+            <div className="navigation-header__links">
+                <NavLink to="/movies" className={setActive}>
+                    Фильмы
+                </NavLink>
+                <NavLink to="/saved-movies" className={setActive}>
+                    Сохраненные фильмы
+                </NavLink>
+            </div>
+            <NavLink to="/profile" className="navigation-header__profile link-opacity">
+                <p className='navigation__profile-text'>Аккаунт</p>
+                <img src={icon}  className="navigation-header__icon" alt="Изображение кнопки Аккаунт"></img>
+            </NavLink>
+        </section>
+    )
 }
 
