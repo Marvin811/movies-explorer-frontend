d5e919fe5af6e9217c71f92d166b7d56d405a7d2
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-06-18 12:05:46 +0300

    feat: добавлен MainApi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Routes, useLocation} from "react-router-dom";
 import './App.css';
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
 import {Header} from "../Header/Header";
 import {Main} from "../Main/Main";
 import {Footer} from "../Footer/Footer";
@@ -12,11 +13,14 @@ import {Profile} from "../Profile/Profile";
 import {NotFoundPage} from "../NotFoundPage/NotFoundPage";
 
 
+
 function App() {
+    const [currentUser, setCurrentUser] = useState({});
     const location = useLocation();
     const pathPageWithHeader = ['/']
     const pathPageWithFooter = ['/', '/movies', '/saved-movies']
     return (
+        <CurrentUserContext.Provider value={currentUser}>
         <div className="page">
             {pathPageWithHeader.includes(location.pathname) && <Header/>}
             <Routes>
@@ -32,8 +36,7 @@ function App() {
 
             {pathPageWithFooter.includes(location.pathname) && <Footer/>}
         </div>
-
-
+        </CurrentUserContext.Provider>
     )
         ;
 }
---
+++ src/utils/MainApi.js
@@ -0,0 +1,125 @@
+class MainApi {
+    constructor({baseUrl, token}) {
+        this._baseUrl = baseUrl;
+        this._token = token;
+    }
+
+    _getResponseDate(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка ${res.status}`);
+    }
+
+    getUserInfo = (token) => {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        })
+            .then(res => this._getResponseDate(res));
+    }
+
+    updateUserInfo(name, email) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            credentials: 'include',
+            method: 'PATCH',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+            })
+        })
+            .then(res => this._getResponseDate(res));
+    }
+
+    createMovie(movieData) {
+        return fetch(`${this._baseUrl}/movies`, {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                country: movieData.country || " ",
+                director: movieData.director,
+                duration: movieData.duration,
+                year: movieData.year,
+                description: movieData.description,
+                trailerLink: movieData.trailerLink || " ",
+                nameRU: movieData.nameRU,
+                nameEN: movieData.nameEN || ' ',
+                image: movieData.image.url,
+                thumbnail: movieData.image.formats.thumbnail.url,
+                movieId: movieData.id.toString(),
+            })
+        })
+            .then(res => this._getResponseDate(res));
+    }
+
+    getSavedMovies() {
+        return fetch(`${this._baseUrl}/movies`, {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+        })
+            .then(res => this._getResponseDate(res));
+    }
+
+    removeMovie(id) {
+        return fetch(`${this._baseUrl}/movies/${id}`, {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+        })
+            .then(res => this._getResponseDate(res));
+    }
+
+    register = (name, email, password) => {
+        return fetch(`${this._baseUrl}/signup`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+            }),
+        })
+            .then(res => this._getResponseDate(res));
+    }
+    authorize = (email, password) => {
+        return fetch(`${this._baseUrl}/signin`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            })
+        })
+            .then(res => this._getResponseDate(res));
+    }
+}
+
+const mainApi = new MainApi({
+    baseUrl: 'ссылка на api'
+})
+
+export default mainApi;
